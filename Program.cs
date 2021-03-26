6c2863e3c9ed8b37994d704308d73507b00d3593
Michael Nikishov <maminpahat@gmail.com>
2021-03-26 23:11:33 +0300

    added a nlog config file; added need dependencies

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using NLog;
 
 namespace Doocutor
 {
     class Program
     {
+        private static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            LOGGER.Info("Start of the program");
             Console.WriteLine("Hello World!");
+            LOGGER.Info("End of the program");
         }
     }
 }
