7817b5fe0b32bb84b17f58fa4bcac3b723a43600
Michael Nikishov <maminpahat@gmail.com>
2021-04-03 12:19:06 +0300

    added an assembly info; minor changes

--- Core/Info.cs
+++ Core/Info.cs
@@ -13,11 +13,17 @@ namespace Doocutor.Core
         /// B - build
         /// DDMMYY - Day.Month.Year datetime
         /// </summary>
-        public static readonly string BuildInfo = "UCB-290321";
-        public static readonly string Updated = "30th of March 2021";
-        public static readonly string Version = "0.0.9";
+        public const string BuildInfo = "UCB-290321";
+        public const string Updated = "30th of March 2021";
+        public const string Company = "Doopath";
+        public const string Version = "0.0.9";
+        public const string ProductName = "Doocutor";
+        public const string ConfigurationAttribute = "Debug";
 
-        public static void ShowDoocutorInfo() => Console.WriteLine($"Doocutor v{Version} with build: {BuildInfo}" +
-            $"updated {Updated}.\nDeveloped in Russia.NizhnyNovgorod 2021.\n");
+        public static string GetDoocutorInfo()
+            => $"Doocutor v{Version} with build: {BuildInfo}" +
+               $"updated {Updated}.\nDeveloped in Russia.NizhnyNovgorod 2021.\n";
+        
+        public static void ShowDoocutorInfo() => Console.WriteLine(GetDoocutorInfo());
     }
 }
---
+++ assemblyinfo.cs
@@ -0,0 +1,10 @@
+
+using Info = Doocutor.Core.Info;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute(Info.Company)]
+[assembly: System.Reflection.AssemblyConfigurationAttribute(Info.ConfigurationAttribute)]
+[assembly: System.Reflection.AssemblyFileVersionAttribute(Info.Version)]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute(Info.Version)]
+[assembly: System.Reflection.AssemblyProductAttribute(Info.ProductName)]
+[assembly: System.Reflection.AssemblyTitleAttribute(Info.ProductName)]
+[assembly: System.Reflection.AssemblyVersionAttribute(Info.Version)]
