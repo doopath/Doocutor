3a31e4da674cc23f8dfd8dca65043c8b64a0bf3b
doopath <doopath@gmail.com>
2021-10-09 14:56:06 +0300

    added a test

--- Tests/Core/CodeFormatters/SourceCodeFormatterTests.cs
+++ Tests/Core/CodeFormatters/SourceCodeFormatterTests.cs
@@ -33,28 +33,34 @@ namespace Tests.Core.CodeFormatters
         public void AdaptCodeForBufferSizeTest()
         {
             var maxLineLength = 50;
-            var prefixLength = 5;
-            var firstLineLength = 72;
+            var firstFourLinesLength = 45;
+            var fifthLineLength = 36;
 
             _formatter.AdaptCodeForBufferSize(maxLineLength);
 
-            var hasTargetLinesCount = _code.Count == 10;
-            var firstLineIsCorrect = _code[0].Length == maxLineLength - prefixLength;
-            var secondLineIsCorrect = _code[1].Length == firstLineLength - (maxLineLength - prefixLength);
+            var hasTargetLinesCount = _code.Count == 8;
+            var firstLineIsCorrect = _code[0].Length == firstFourLinesLength;
+            var secondLineIsCorrect = _code[1].Length == firstFourLinesLength;
+            var thirdLineIsCorrect = _code[2].Length == firstFourLinesLength;
+            var fourthLineIsCorrect = _code[3].Length == firstFourLinesLength;
+            var fifthLineIsCorrect = _code[4].Length == fifthLineLength;
 
             Console.WriteLine(string.Join("\n", _code));
 
             Assert.True(hasTargetLinesCount, "Adapted code has incorrect line number!");
             Assert.True(firstLineIsCorrect, "First line of code is incorrect!");
             Assert.True(secondLineIsCorrect, "Second line of code is incorrect");
+            Assert.True(thirdLineIsCorrect, "Third line of code is incorrect");
+            Assert.True(fourthLineIsCorrect, "Fourth line of code is incorrect");
+            Assert.True(fifthLineIsCorrect, "Fifth line of code is incorrect");
         }
 
         private void FillCode()
         {
-            _code.Add("------------------------------------------------------------------------");
-            _code.Add("---------------------------------------------------------");
-            _code.Add("-------------------------------------------");
-            _code.Add("------------------------------");
+            _code.Add("--------------------------------------------------------");
+            _code.Add("------------------------------------------------------");
+            _code.Add("------------------------------------------------------------");
+            _code.Add("---------------------------------------------");
             _code.Add("-");
             _code.Add(" ");
             _code.Add("\\");
