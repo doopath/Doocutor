839acea18db06a76319580307af01fd9220926dd
Michael Nikishov <doopath@gmail.com>
2021-10-24 11:49:10 -0700

    minor changes

--- DynamicEditor/Core/CuiRender.cs
+++ DynamicEditor/Core/CuiRender.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using Domain.Core.CodeBuffers;
 using Domain.Core.OutBuffers;
 using Domain.Core.Scenes;
